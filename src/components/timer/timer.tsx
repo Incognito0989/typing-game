ea7c692459aeb435c0c08a988ebb60dbb93cb284
andrew.jones <andrew109jones@gmail.com>
2022-09-07 20:58:50 -0400

    Start adding timer; Make input hidden and on top of expected text

---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,14 @@
+import './timer.css';
+import { Component } from 'react';
+
+class Timer extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+  
+export default Timer;
